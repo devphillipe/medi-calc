86d92013dc4e35b08c850637cd67e448151481d7
Phillipe Lacerda <117359249+devphillipe@users.noreply.github.com>
2024-12-19 10:11:18 -0300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,7 +20,7 @@ export default function Form({ onSubmit }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4 p-4">
       <input
         type="text"
         placeholder="Nome do remédio"
